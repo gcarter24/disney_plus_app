c1f469d9345796a4673884325ba6975028d9599a
George <gchriscarter@gmail.com>
2022-05-10 16:31:46 -0400

    initial setup

---
+++ app/controllers/titles_controller.rb
@@ -0,0 +1,51 @@
+class TitlesController < ApplicationController
+  before_action :set_title, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @titles = Title.all.order(:name => :asc)
+  end
+
+  def show
+  end
+
+  def new
+    @title = Title.new
+  end
+
+  def edit
+  end
+
+  def create
+    @title = Title.new(title_params)
+    if @title.save
+      flash[:notice] = "Title was created successfully"
+      redirect_to title_path(@title)
+    else
+      render "new"
+    end
+  end
+
+  def update
+    if @title.update(title_params)
+      flash[:notice] = "Title was updated successfully"
+      redirect_to @title
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @title.destroy
+    redirect_to titles_path
+  end
+
+  private
+
+  def set_title
+    @title = Title.find_by(id: params[:id])
+  end
+
+  def title_params
+    params.require(:title).permit(:name, :description, :director, :cast, :country, :release_year, :rating, :duration, :media_type)
+  end
+end
